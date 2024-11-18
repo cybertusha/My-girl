16355bbba9cf32e789e84a6ac9d991d7777d1305
Beast PJ <p.jadhav61657@gmail.com>
2024-11-18 18:47:06 +0530

    Hearts Popups

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const envelope = document.querySelector('.envelope-wrapper');
 const heart = document.querySelector('.heart');
+const hearts = document.querySelector('.hearts');
+
 
 heart.addEventListener('mouseenter', () => {
     envelope.classList.add('flap');
